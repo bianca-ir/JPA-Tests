bbe5ad9fb3a69ad9fc2c2e695354dcdefdec2ed0
bianca-ir <bianca.iii99@gmail.com>
2024-02-21 01:32:24 +0200

    add quiz servlet

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -11,25 +11,28 @@ import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
-    //   UserDAO userDAO = new UserDAOImpl();
+   
+//
+//      QuestionDAO questionDAO = new QuestionDAO(); 
+//      QuizDAO quizDAO = new QuizDAO(); 
+//      Quiz quiz = new Quiz();
+//      
+//      Question newQuestion = new Question();
+//      newQuestion.setQText("what is the result for 2+2?");
+//      newQuestion.setAns(4);
+//      String[] options = new String[] {"10", "100", "7", "4"};
+//      newQuestion.setOptions(options);
+//      
+//      
+//      Collection<Question> questionCollection = new ArrayList<>();
+//      questionCollection.add(newQuestion);
+//        
+//      quiz.setNumberOfQuestions(2);
+//      quiz.setQuestionArray(questionCollection );
+//      questionDAO.insertQuestion(newQuestion);
+//      quizDAO.insertQuiz(quiz);
+//        
 
-      QuestionDAO questionDAO = new QuestionDAO(); 
-      QuizDAO quizDAO = new QuizDAO(); 
-      Quiz quiz = new Quiz();
-      
-      Question newQuestion = new Question();
-      newQuestion.setQText("what is the result for 2+2?");
-      newQuestion.setAns(4);
-      Collection<Question> questionCollection = new ArrayList<>();
-      questionCollection.add(newQuestion);
-        
-      quiz.setNumberOfQuestions(2);
-      quiz.setQuestionArray(questionCollection );
-      questionDAO.insertQuestion(newQuestion);
-      quizDAO.insertQuiz(quiz);
-        
-
-       // userDAO.insertUser("admin", "pass");
 
      
     }
